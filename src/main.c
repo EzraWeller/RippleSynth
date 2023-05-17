33ab6286ea95f7f7affc22ec9b2107bc7e84122f
Ezra Weller <ezra.weller@gmail.com>
2023-05-17 08:09:42 -0400

    pdxinfo

--- src/main.c
+++ src/main.c
@@ -610,15 +610,6 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
     return 0;
 }
 
-
-#define TEXT_WIDTH 86
-#define TEXT_HEIGHT 16
-
-int x = (400-TEXT_WIDTH)/2;
-int y = (240-TEXT_HEIGHT)/2;
-int dx = 1;
-int dy = 2;
-
 static void processInputs(void)
 {
     PDButtons current;
